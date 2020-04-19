3bdbfa88881ffc30aec3275ce62fcb62e3d588ad
srasetti <srasetti@yahoo.com>
2020-04-19 19:26:25 -0400

    removed virtual env files

--- start.py
+++ start.py
@@ -1,5 +1,17 @@
+import scripts
+
+
 print("welcome to RPG Text")
 print("-------------------")
 print("Write Start to start or Help to learn how to play")
 menuchoice = input("Choose:")
-print(menuchoice)
+
+if menuchoice.lower() == "start" :  
+    ennemy_str = 100
+    result = scripts.fight(ennemy_str)
+    print(result)
+elif menuchoice.lower() == "help" :
+    print("Ok let me help")
+else :
+    print("typo or wrong choice")
+#print(menuchoice)
